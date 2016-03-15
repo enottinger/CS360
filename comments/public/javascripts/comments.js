7688d65a563f0eb7e24606da783616766e1481f2
enottinger <enottinger@gmail.com>
2016-03-15 04:06:39 +0000

    Lab 8

--- comments/public/javascripts/comments.js
+++ comments/public/javascripts/comments.js
@@ -1,9 +1,83 @@
 var url = "comment";
-$.ajax({
-	url:url,
-	type: "POST",
-	data: jobj,
-	contentType: "application/json; charset -utf-8",
-	success: function(data,textStatus) {
-		$("#done").html
-	}
+$(document).ready(function(){
+    $("#serialize").click(function(){
+        var myobj = {Name:$("#Name").val(),Comment:$("#Comment").val()};
+        jobj = JSON.stringify(myobj);
+        $("#json").text(jobj);
+	var url = "comment";
+	$.ajax({
+	  url:url,
+	  type: "POST",
+	  data: jobj,
+	  contentType: "application/json; charset=utf-8",
+	  success: function(data,textStatus) {
+	      $("#done").html(textStatus);
+	  }
+	})
+    });
+    $("#getThem").click(function() {
+      $.getJSON('comment', function(data) {
+        console.log(data);
+        var everything = "<ul>";
+        for(var comment in data) {
+          com = data[comment];
+          everything += "<li>Name: " + com.Name + " -- Comment: " + com.Comment + "</li>";
+        }
+        everything += "</ul>";
+        $("#comments").html(everything);
+      })
+   });
+$("#register").click(function(){
+        var myobj = {Username:$("#Username").val(),AvatarURL:$("#Avatar").val()};
+        jobj = JSON.stringify(myobj);
+        $("#jsonU").text(jobj);
+	var url = "users";
+	$.ajax({
+	  url:url,
+	  type: "POST",
+	  data: jobj,
+	  contentType: "application/json; charset=utf-8",
+	  success: function(data,textStatus) {
+	      $("#doneU").html(textStatus);
+	  }
+	})
+    });
+    $("#getUsers").click(function() {
+       $.getJSON('users', function(data) {
+        console.log(data);
+        var everything = "<ul>";
+        for(var user in data) {
+          us = data[user];
+          everything += "<li>Name: " + us.Username + "<br><img style=\"max-width: 100px\" src = \"" + us.AvatarURL + "\"/>";
+          everything += "<div id=\"" + us.Username+ "\"></div></li>"
+	  
+        }
+        everything += "</ul>";
+	everything += "<br><button id=\"loadComments\">Get Comments</button>";
+	console.log(everything);
+        $("#users").html(everything);
+      });
+   
+   });
+
+   $("#loadComments").click(function(data) {
+      $.getJSON('comment', function(cdata){
+	      console.log(cdata);
+	      var username = us.userName
+              var everything2 = "<ul>";
+       	      for(var comment in cdata) {
+        	 com = cdata[comment];
+     
+
+		   everything2 = "<li>Comment: " + com.Comment + "</li>";
+		    $("#"+com.Name).append(everything2);
+		
+       	      }
+             
+	      console.log(everything2);
+	  });   
+   });
+
+});
+
+
--- comments/routes/index.js
+++ comments/routes/index.js
@@ -1,6 +1,42 @@
 var express = require('express');
 var router = express.Router();
 
+
+/* Set up mongoose in order to connect to mongo database */
+var mongoose = require('mongoose'); //Adds mongoose as a usable dependency
+
+mongoose.connect('mongodb://localhost/commentDB'); //Connects to a mongo database called "commentDB"
+
+var commentSchema = mongoose.Schema({ //Defines the Schema for this database
+  Name: String,
+  Comment: String
+});
+
+var Comment = mongoose.model('Comment', commentSchema); //Makes an object from that schema as a model
+
+var db = mongoose.connection; //Saves the connection as a variable to use
+db.on('error', console.error.bind(console, 'connection error:')); //Checks for connection errors
+db.once('open', function() { //Lets us know when we're connected
+  console.log('Connected');
+});
+
+mongoose.connect('mongodb://localhost/comUserDB'); //Connects to a mongo database called "comUserDB"
+
+var userSchema = mongoose.Schema({ //Defines the Schema for this database
+  Username: String,
+  AvatarURL: String
+});
+
+var User = mongoose.model('User', userSchema); //Makes an object from that schema as a model
+
+var dbU = mongoose.connection; //Saves the connection as a variable to use
+dbU.on('error', console.error.bind(console, 'connection error:')); //Checks for connection errors
+dbU.once('open', function() { //Lets us know when we're connected
+  console.log('Connected to User Database');
+});
+
+
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
@@ -9,7 +45,55 @@ router.get('/', function(req, res, next) {
 router.post('/comment', function(req, res, next) {
   console.log("POST comment route");
   console.log(req.body);
-  res.sendStatus(200);
+
+  var newcomment = new Comment(req.body); //[3]
+  console.log(newcomment); //[3]
+  newcomment.save(function(err, post) { //[4]
+    if (err) return console.error(err);
+    console.log(post);
+    res.sendStatus(200);
+  });
+
+});
+
+/* GET comments from database */
+router.get('/comment', function(req, res, next) {
+  console.log("In the GET route?");
+  Comment.find(function(err,commentList) { //Calls the find() method on your database
+    if (err) return console.error(err); //If there's an error, print it out
+    else {
+      console.log(commentList); //Otherwise console log the comments you found
+      res.json(commentList);
+      
+    }
+  })
+});
+
+router.post('/users', function(req, res, next) {
+  console.log("POST comment route");
+  console.log(req.body);
+
+  var newcomment = new User(req.body); //[3]
+  console.log(newcomment); //[3]
+  newcomment.save(function(err, post) { //[4]
+    if (err) return console.error(err);
+    console.log(post);
+    res.sendStatus(200);
+  });
+
+});
+
+/* GET comments from database */
+router.get('/users', function(req, res, next) {
+  console.log("In the GET route?");
+  User.find(function(err,userList) { //Calls the find() method on your database
+    if (err) return console.error(err); //If there's an error, print it out
+    else {
+      console.log(userList); //Otherwise console log the comments you found
+      res.json(userList);
+      
+    }
+  })
 });
 
 module.exports = router;
